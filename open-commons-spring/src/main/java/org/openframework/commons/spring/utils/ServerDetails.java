3d20e5faefe0f7bf84253d3316144e2030f99285
helloprav <helloprav@gmail.com>
2022-01-14 20:22:09 +0530

    tomcat access logs added

--- open-commons-spring/src/main/java/org/openframework/commons/spring/utils/ServerDetails.java
+++ open-commons-spring/src/main/java/org/openframework/commons/spring/utils/ServerDetails.java
@@ -1,35 +1,26 @@
 package org.openframework.commons.spring.utils;
 
-
-
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServerDetails {
-	public ServletWebServerApplicationContext getServer() {
-		return server;
-	}
-
-	public void setServer(ServletWebServerApplicationContext server) {
-		this.server = server;
-	}
 
 	@Autowired
-	private ServletWebServerApplicationContext server;
+	private ServerProperties serverProperties;
 
 	public Map<String, String> getServerDetails() {
 
-		String schema = "http";					// currently hard-coded
-		String serverName = "localhost";		// currently hard-coded
-		int port = this.getServer().getWebServer().getPort();
+		String schema = "http"; // currently hard-coded
+		String serverName = "localhost"; // currently hard-coded
+		int port = serverProperties.getPort();
 		String contextPath = ApplicationContextProvider.getApplicationContext().getApplicationName();
-		String applicationUrl = schema+"://"+serverName+":"+port+contextPath;
-		
+		String applicationUrl = schema + "://" + serverName + ":" + port + contextPath;
+
 		Map<String, String> serverInfos = new HashMap<String, String>();
 		serverInfos.put("schema", schema);
 		serverInfos.put("serverName", serverName);
